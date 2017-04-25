fd3d09f61ecc28ae0fb8c24a6638a7c9c767ca3b
Erik Teichmann <erik-teichmann@gmx.de>
2017-04-25 16:38:46 +0200

    Calculate the evolution of modes and save them to file

--- src/modes.cpp
+++ src/modes.cpp
@@ -16,3 +16,40 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include <cstdio>
+#include <vector>
+#include <string>
+#include "../lib/kur_siv.h"
+
+using namespace std;
+
+int main() {
+  const double h = 0.001;
+  const double L = 10.;
+  const int N = 30;
+  const int n = 1000;
+  FILE * datafile = fopen("data/modes.dat", "w");
+
+  fprintf(datafile, "# h= %.9f , L= %.9f , N= %d\n", h, L, N);
+
+  KuramotoSivashinsky kuramoto(h, L, N);
+  vector<complex<double> > C = kuramoto.GetC();
+  fprintf(datafile, "%.9f %.9f", C[0].real(), C[0].imag());
+  for (int i = 1; i < C.size(); ++i) {
+    fprintf(datafile, " %.9f %.9f", C[i].real(), C[i].imag());
+  }
+  fprintf(datafile, "\n");
+  fflush(datafile);
+  for (int i = 0; i < n; ++i) {
+    kuramoto.Integrate();
+    C = kuramoto.GetC();
+    fprintf(datafile, "%.9f %.9f", C[0].real(), C[0].imag());
+    for (int j = 1; j < C.size(); ++j) {
+      fprintf(datafile, " %.9f %.9f", C[j].real(), C[j].imag());
+    }
+    fprintf(datafile, "\n");
+    fflush(datafile);
+  }
+  fclose(datafile);
+}
