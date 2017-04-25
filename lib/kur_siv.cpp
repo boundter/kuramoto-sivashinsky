7af0dcdfc0dc1b0c4a634272182e6612d3427ff9
Erik Teichmann <erik-teichmann@gmx.de>
2017-04-25 13:44:05 +0200

    Add license spoiler and comments

--- lib/kur_siv.cpp
+++ lib/kur_siv.cpp
@@ -1,3 +1,23 @@
+// This file kur_siv.cpp is part of kuramoto-sivashinksy.
+//
+// It contains the class KuramotoSivashinsky to model the equation.
+//
+// Copyright (C) 2017 Erik Teichmann (eteichma@uni-potsdam.de)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 #include "kur_siv.h"
 
 KuramotoSivashinsky::KuramotoSivashinsky(double step, double L,
--- lib/kur_siv.h
+++ lib/kur_siv.h
@@ -1,3 +1,22 @@
+// This file kur_siv.h is part of kuramoto-sivashinksy.
+//
+// It contains the header for class KuramotoSivashinsky.
+//
+// Copyright (C) 2017 Erik Teichmann (eteichma@uni-potsdam.de)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef __KUR_SIV__
 #define __KUR_SIV__
 
@@ -9,13 +28,13 @@
 using namespace std;
 
 class KuramotoSivashinsky {
-  vector<complex<double> > C;
-  int N;
-  double h;
-  double K;
-  vector<double> a;
-  vector<double> eah;
-  vector<int> modes;
+  vector<complex<double> > C; // Fourier modes
+  int N; // number of modes to consider
+  double h; // time-step
+  double K; // wave-vector = 2*pi/L
+  vector<double> a; // a = n^2*K^2 - n^4*K^4
+  vector<double> eah; // eah = exp(a*h)
+  vector<int> modes; // evolving modes (a, C =/= 0)
 
 public:
   KuramotoSivashinsky(double, double, int);
@@ -23,6 +42,7 @@ public:
   vector<double> Geta() {return a;}
   vector<double> Geteah() {return eah;}
   vector<int> Getmodes() {return modes;}
+  vector<double> Getu(double);
 };
 
 #endif
