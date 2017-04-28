23f700b06565ccf941b066f6f133ebb3ec746bd6
Erik Teichmann <erik-teichmann@gmx.de>
2017-04-28 09:23:09 +0200

    Add plot for particle advection

---
+++ plt/particles.py
@@ -0,0 +1,39 @@
+# This file particles.py is part of kuramoto-sivashinksy.
+#
+# It plots the evolution of particles on the veclocity-field.
+#
+# Copyright (C) 2017 Erik Teichmann (eteichma@uni-potsdam.de)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+sys.path.insert(0, "lib")  # for import of fileoperations
+import numpy as np
+import matplotlib.pyplot as plt
+from fileoperations import ReadFile
+
+# Define size of the plot for the rport
+plt.rcParams["figure.figsize"] = [8, 5]
+plt.rcParams["font.size"] = 17
+
+header, particles = ReadFile("data/particles.dat", 1, " ")
+
+fig, ax = plt.subplots()
+for i in range(1, particles.shape[0]):
+    abs_d_data = np.abs(np.diff(particles[i]))
+    mask = np.hstack([ abs_d_data > abs_d_data.mean()+3*abs_d_data.std(), [False]])
+    masked_data = np.ma.MaskedArray(particles[i], mask)
+    ax.plot(particles[0], masked_data)
+ax.set_ylim(0., header["L"])
+plt.show()
